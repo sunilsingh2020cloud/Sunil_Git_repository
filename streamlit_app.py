9d1ddb4f244876f13114c2d55593cc2c64ba620c
Sunil Singh <108504874+sunilsingh2020cloud@users.noreply.github.com>
2022-07-10 22:58:39 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,8 +16,8 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 
 # Let's put a pick list here so they can pick the fruit they want to include 
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Banana'])
-# fruits_to_show=my_fruit_list.loc[fruits_selected]
-my_cur.execut("insert into fruit_load_list values('from streamlit')")
+ fruits_to_show=my_fruit_list.loc[fruits_selected]
+#my_cur.execut("insert into fruit_load_list values('from streamlit')")
 
 streamlit.dataframe(fruits_to_show);
 
@@ -28,8 +28,8 @@ streamlit.write('The user entered ', fruit_choice)
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
-# streamlit.text(fruityvice_response.json())
-my_cur.execut("insert into fruit_load_list values('from streamlit')")
+ streamlit.text(fruityvice_response.json())
+# my_cur.execut("insert into fruit_load_list values('from streamlit')")
 
 # write your own comment -what does the next line do? 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
@@ -50,5 +50,5 @@ streamlit.dataframe(my_data_rows)
 
 add_my_fruit = streamlit.text_input('What fruit would you like to add?','Jackfruit')
 streamlit.write('The user entered ', add_my_fruit)
-my_cur.execut("insert into fruit_load_list values('from streamlit')")
+#my_cur.execut("insert into fruit_load_list values('from streamlit')")
 
